04b6ab4173904937c44bd966d0568941c1c24fc5
kakawi <kakawi13@gmail.com>
2020-04-26 13:08:10 +0300

    added multiplier which depends on tick

--- src/main/java/ru/codebattle/client/handled/calculator/PathValueCalculator.java
+++ src/main/java/ru/codebattle/client/handled/calculator/PathValueCalculator.java
@@ -7,25 +7,113 @@ import ru.codebattle.client.handled.TypedBoardPoint;
 public class PathValueCalculator {
 
 	public double calculateValueForStep(TypedBoardPoint nextPoint, int tick) {
+		switch (tick) {
+			case 0:
+				return firstTick(nextPoint);
+			case 1:
+				return secondTick(nextPoint);
+			case 2:
+				return thirdTick(nextPoint);
+			case 3:
+				return forthTick(nextPoint);
+			default:
+				return othersTick(nextPoint);
+		}
+	}
+
+	private double firstTick(TypedBoardPoint nextPoint) {
 		ExplosionInfo explosionInfo = nextPoint.getExplosionInfo();
 		ExplosionStatus explosionStatus = explosionInfo.getStatus();
-		int points;
+		int price;
 		switch (explosionStatus) {
 			case AFTER_4_TICKS:
-				points = 100;
+				price = 100;
 				break;
 			case AFTER_3_TICKS:
-				points = 200;
+				price = 200;
 				break;
 			case AFTER_2_TICKS:
-				points = 300;
+				price = 300;
 				break;
 			case NEXT_TICK:
 				return Double.MAX_VALUE;
 			default:
-				points = 30;
+				price = 30;
 		}
 
-		return points;
+		return price;
+	}
+
+	private double secondTick(TypedBoardPoint nextPoint) {
+		ExplosionInfo explosionInfo = nextPoint.getExplosionInfo();
+		ExplosionStatus explosionStatus = explosionInfo.getStatus();
+		int price;
+		switch (explosionStatus) {
+			case AFTER_4_TICKS:
+				price = 80;
+				break;
+			case AFTER_3_TICKS:
+				price = 160;
+				break;
+			case AFTER_2_TICKS:
+				price = 200;
+				break;
+			case NEXT_TICK:
+				return 1000;
+			default:
+				price = 30;
+		}
+
+		return price;
+	}
+
+	private double thirdTick(TypedBoardPoint nextPoint) {
+		ExplosionInfo explosionInfo = nextPoint.getExplosionInfo();
+		ExplosionStatus explosionStatus = explosionInfo.getStatus();
+		int price;
+		switch (explosionStatus) {
+			case AFTER_4_TICKS:
+				price = 70;
+				break;
+			case AFTER_3_TICKS:
+				price = 150;
+				break;
+			case AFTER_2_TICKS:
+				price = 180;
+				break;
+			case NEXT_TICK:
+				return 500;
+			default:
+				price = 30;
+		}
+
+		return price;
+	}
+
+	private double forthTick(TypedBoardPoint nextPoint) {
+		ExplosionInfo explosionInfo = nextPoint.getExplosionInfo();
+		ExplosionStatus explosionStatus = explosionInfo.getStatus();
+		int price;
+		switch (explosionStatus) {
+			case AFTER_4_TICKS:
+				price = 60;
+				break;
+			case AFTER_3_TICKS:
+				price = 130;
+				break;
+			case AFTER_2_TICKS:
+				price = 150;
+				break;
+			case NEXT_TICK:
+				return 200;
+			default:
+				price = 30;
+		}
+
+		return price;
+	}
+
+	private double othersTick(TypedBoardPoint nextPoint) {
+		return 30;
 	}
 }
