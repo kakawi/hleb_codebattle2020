6f7097ce139836e773a5e0fc1f9b5397b5689abf
kakawi <kakawi13@gmail.com>
2020-04-27 22:42:49 +0300

    added SpringBoot and vanila java starters

--- src/main/java/ru/codebattle/client/Main.java
+++ src/main/java/ru/codebattle/client/Main.java
@@ -1,13 +1,31 @@
 package ru.codebattle.client;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.codebattle.client.handled.TickHandler;
+import ru.codebattle.client.handled.calculator.PathCalculator;
+import ru.codebattle.client.handled.calculator.PathValueCalculator;
+import ru.codebattle.client.handled.strategy.move.DestinationStrategyManager;
+import ru.codebattle.client.handled.strategy.move.NearestBombermanStrategy;
+import ru.codebattle.client.handled.strategy.plant.BombsController;
+import ru.codebattle.client.handled.strategy.plant.BombsControllerImpl;
+import ru.codebattle.client.handled.strategy.plant.PlantStrategiesManager;
+import ru.codebattle.client.handled.strategy.plant.SmartPlantStrategy;
+import ru.codebattle.client.history.History;
+import ru.codebattle.client.history.HistoryImpl;
 
-@SpringBootApplication
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Main.class);
-        app.run(args);
+    public static void main(String[] args) throws URISyntaxException {
+        DestinationStrategyManager destinationStrategyManager = new DestinationStrategyManager(Arrays.asList(new NearestBombermanStrategy()));
+        PathCalculator pathCalculator = new PathCalculator(new PathValueCalculator());
+        BombsController bombsController = new BombsControllerImpl();
+        PlantStrategiesManager plantStrategiesManager = new PlantStrategiesManager(Arrays.asList(new SmartPlantStrategy(bombsController)));
+        History history = new HistoryImpl();
+        TickHandler tickHandler = new TickHandler(destinationStrategyManager, pathCalculator, plantStrategiesManager, bombsController, history);
+
+        CodeBattleClient codeBattleClient = new CodeBattleClient("http://codebattle2020s1.westeurope.cloudapp.azure.com/codenjoy-contest/board/player/3f1pmuv22xboeomi7ts3?code=5836526866745355424");
+        codeBattleClient.run(tickHandler::handle);
     }
 }
